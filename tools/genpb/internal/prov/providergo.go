a518c08fb7b3f2fc1480059b835e41be70f8a97a
buf1024 <heidonglgc@126.com>
2017-01-15 20:41:22 +0800

    go provider

--- tools/genpb/internal/prov/providergo.go
+++ tools/genpb/internal/prov/providergo.go
@@ -1,15 +1,17 @@
 package prov
 
 import (
+	"bytes"
 	"fmt"
 	"io/ioutil"
 	"os"
 	"strconv"
 	"strings"
+	"text/template"
 )
 
 type goProtoData struct {
-	cmommand    int64
+	command     int64
 	comments    string
 	lastComment string
 	msg         string
@@ -22,11 +24,26 @@ type goProto struct {
 }
 
 type goRrovider struct {
-	proto goProto
+	proto    goProto
+	template string
 }
 
 func (p *goRrovider) GenCmdFile(protoFile string) (string, error) {
-	return "Hello", nil
+	err := p.parseProto(protoFile)
+	if err != nil {
+		return "", err
+	}
+
+	report, err := template.New("genfile").Parse(p.template)
+	if err != nil {
+		return "", fmt.Errorf("parse template failed, err = %s", err)
+	}
+	var output bytes.Buffer
+	err = report.Execute(&output, p.proto)
+	if err != nil {
+		return "", fmt.Errorf("rend template failed, err = %s", err)
+	}
+	return output.String(), nil
 }
 
 func (p *goRrovider) parseProto(protoFile string) error {
@@ -51,7 +68,7 @@ func (p *goRrovider) parseProto(protoFile string) error {
 func (p *goRrovider) parseLine(line string) error {
 	line = strings.Trim(line, "\r\n\t ;")
 	switch {
-	case line == "package":
+	case strings.HasPrefix(line, "package"):
 		packString := strings.Split(line, " ")
 		if len(packString) != 2 {
 			return fmt.Errorf("package statement missing package name, line = %s", line)
@@ -67,22 +84,97 @@ func (p *goRrovider) parseLine(line string) error {
 		if len(lastComment) <= 0 {
 			return fmt.Errorf("missing message command comment info, line = %s", line)
 		}
-		comment := strings.Split(line, " ")
-		if len(comment) != 2 {
-			return fmt.Errorf("format style not right, line = %s", line)
+		idx := strings.LastIndex(lastComment, "0x")
+		if idx < 0 {
+			return fmt.Errorf("comment style not right, line = %s, comment = %s", line, lastComment)
 		}
-		command, err := strconv.ParseInt(strings.Trim(comment[1], " "), 16, 64)
+		strCommand := strings.Trim(lastComment[idx:len(lastComment)], " ")
+		command, err := strconv.ParseInt(strCommand, 0, 64)
 		if err != nil {
-			return fmt.Errorf("format style not right, line = %s, strconv error = %s", line, err)
+			return fmt.Errorf("comment style not right, line = %s, str = %s, strconv error = %s", line, strCommand, err)
 		}
 		if command == 0x0 {
 			p.proto.curData = goProtoData{}
+			return nil
 		}
+		p.proto.curData.command = command
+		msgs := strings.Split(line, " ")
+		if len(msgs) < 2 {
+			return fmt.Errorf("message style not right, line = %s", line)
+		}
+		idx = strings.LastIndex(msgs[1], "{")
+		if idx < 0 {
+			idx = len(msgs[1])
+		}
+		msg := strings.Trim(msgs[1][0:idx], " ")
+		p.proto.curData.msg = msg
 
+		p.proto.data = append(p.proto.data, p.proto.curData)
+
+		p.proto.curData = goProtoData{}
 	}
+	return nil
 
 }
 
 func init() {
-	Register("go", &goRrovider{})
+	var template = `
+package {{.pack}}
+
+import (
+	"fmt"
+
+	pb "github.com/golang/protobuf/proto"	
+)
+
+type CommandCode int64
+
+const (
+	{{rang .data}}
+	{{.comments}}
+	CMD_{{strings.Upper(.msg)}} CommandCode = {{.command}} {{.lastComment}}
+	{{end}}
+)
+
+var message map[int64]pb.Message
+
+func Message(command int64) (pb.Message, error) {
+	if m, ok := message[command]; ok {
+		pb.Clone(m)
+		return m, nil
+	}
+	return nil, fmt.Errorf("command %d not found", command)
+}
+
+func Parse(command int64, buf []byte) (pb.Message, error) {
+	msg, err := Message(command)
+	if err != nil {
+		return nil, err
+	}
+	err = pb.Unmarshal(buf, msg)
+	if err != nil {
+		return nil, err
+	}
+	return msg, nil
+}
+
+func Serialize(msg pb.Message) ([]byte, error) {
+	return pb.Marshal(msg)
+}
+
+func Debug(command int64, msg pb.Message) string {
+	return fmt.Sprintf("command:0x%x %s", command, pb.CompactTextString(msg))
+}
+
+func init() {
+	message = make(map[int64]pb.Message)
+
+	{{rang .data}}
+	message[CMD_{{strings.Upper(.msg)}}] = &{{.msg}}{} {{.lastComment}}
+	{{end}}
+}
+`
+	provider := &goRrovider{}
+	provider.template = template
+	Register("go", provider)
 }
