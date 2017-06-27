b53efc95e21630da924ebd3a6f2ddbeb5ecf3c20
luoguochun <450171094@qq.com>
2017-06-27 10:36:25 +0800

    packet endecrypt

--- util/crypt.go
+++ util/crypt.go
@@ -18,92 +18,161 @@ type Caesar struct {
 }
 
 func (c *Caesar) Init() {
-	c.UC_DECRYPT_CHARS = make([]byte, 26)
-	c.LC_DECRYPT_CHARS = make([]byte, 26)
+	UC_DECRYPT_CHARS = make([]byte, 26)
+	LC_DECRYPT_CHARS = make([]byte, 26)
 
-	c.UC_ENCRYPT_CHARS = []byte{'M', 'D', 'X', 'U', 'P', 'I', 'B', 'E', 'J', 'C', 'T', 'N',
+	UC_ENCRYPT_CHARS = []byte{'M', 'D', 'X', 'U', 'P', 'I', 'B', 'E', 'J', 'C', 'T', 'N',
 		'K', 'O', 'G', 'W', 'R', 'S', 'F', 'Y', 'V', 'L', 'Z', 'Q', 'A', 'H'}
 
-	c.LC_ENCRYPT_CHARS = []byte{'m', 'd', 'x', 'u', 'p', 'i', 'b', 'e', 'j', 'c', 't', 'n',
+	LC_ENCRYPT_CHARS = []byte{'m', 'd', 'x', 'u', 'p', 'i', 'b', 'e', 'j', 'c', 't', 'n',
 		'k', 'o', 'g', 'w', 'r', 's', 'f', 'y', 'v', 'l', 'z', 'q', 'a', 'h'}
 	for i := 0; i < 26; i++ {
-		b := c.UC_ENCRYPT_CHARS[i]
-		c.UC_DECRYPT_CHARS[b-'A'] = (byte)('A' + i)
-		b = c.LC_ENCRYPT_CHARS[i]
-		c.LC_DECRYPT_CHARS[b-'a'] = (byte)('a' + i)
+		b := UC_ENCRYPT_CHARS[i]
+		UC_DECRYPT_CHARS[b-'A'] = (byte)('A' + i)
+		b = LC_ENCRYPT_CHARS[i]
+		LC_DECRYPT_CHARS[b-'a'] = (byte)('a' + i)
 	}
 }
 func (c *Caesar) Encrypt(b byte) byte {
 	if b >= 'A' && b <= 'Z' {
-		return c.UC_ENCRYPT_CHARS[b-'A']
+		return UC_ENCRYPT_CHARS[b-'A']
 	} else if b >= 'a' && b <= 'z' {
 
-		return c.LC_ENCRYPT_CHARS[b-'a']
+		return LC_ENCRYPT_CHARS[b-'a']
 	}
 
 	return b
 }
 func (c *Caesar) Decrypt(b byte) byte {
 	if b >= 'A' && b <= 'Z' {
-		return c.UC_DECRYPT_CHARS[b-'A']
+		return UC_DECRYPT_CHARS[b-'A']
 	} else if b >= 'a' && b <= 'z' {
-		return c.LC_DECRYPT_CHARS[b-'a']
+		return LC_DECRYPT_CHARS[b-'a']
 	}
 	return b
 }
 func (c *Caesar) EncryptBytes(b []byte) []byte {
 	dst := make([]byte, len(b))
 	for i := 0; i < len(b); i++ {
-		dst[i] = c.Encrypt(b[i])
+		dst[i] = Encrypt(b[i])
 	}
 	return dst
 }
 func (c *Caesar) DecryptBytes(b []byte) []byte {
 	dst := make([]byte, len(b))
 	for i := 0; i < len(b); i++ {
-		dst[i] = c.Decrypt(b[i])
+		dst[i] = Decrypt(b[i])
 	}
 	return dst
 }
 */
-type Crypt struct {
-	//cae *Caesar
-}
 
-func NewCrypt() *Crypt {
-	c := &Crypt{}
+var des3DefKey []byte = []byte("ZGFyayBtYXR0ZXIgZXNjYXBl")
+var des3DefIov []byte = []byte("bmV2ZXI=")
 
-	return c
-}
-
-func (c *Crypt) getDBKey() string {
+func getDBKey() string {
 	s := math.Sqrt(17.0)
 	key := fmt.Sprintf("%.22f", s)
 	return key[:8]
 }
+func getPacketKey(key string) ([]byte, []byte, error) {
+	keyByte := make([]byte, 24)
+	if len(key) > 0 {
+		len := len(key)
+		if len < 8 || len > 24 {
+			return nil, nil, fmt.Errorf("key's len must >=8 && <= 24")
+		}
+		copy(keyByte, []byte(key))
+		rst := 24 - len
+		for i := 0; i < rst; i++ {
+			keyByte[len+i] = byte(0)
+		}
+	}
+	return keyByte, des3DefIov, nil
 
-func (c *Crypt) PacketEncrypt(buf []byte) ([]byte, error) {
-	return buf, nil
+}
+func noStdPadding(ciphertext []byte, blockSize int) []byte {
+	bodylen := len(ciphertext)
+	append := bodylen % blockSize
+	if append > 0 {
+		append = 8 - append
+		bodylen = bodylen + append
+	}
+	padding := make([]byte, bodylen)
+	copy(padding, ciphertext)
+	for i := 0; i < append; i++ {
+		padding[bodylen-append+i] = byte(0)
+	}
+	return padding
 }
 
-func (c *Crypt) PacketDecrypt(buf []byte) ([]byte, error) {
-	return buf, nil
+func noStdUnpadding(origData []byte) []byte {
+	length := len(origData)
+	unpadding := 0
+	for i := length - 1; i >= length-8; i-- {
+		if origData[i] == byte(0) {
+			unpadding++
+		} else {
+			break
+		}
+	}
+	return origData[:(length - unpadding)]
 }
 
-func (c *Crypt) PswEncrypt(pass string, signacct string) (string, error) {
+func PacketEncrypt(key string, buf []byte) ([]byte, error) {
+	keyBytes, iovBytes, err := getPacketKey(key)
+	if err != nil {
+		return nil, err
+	}
+	fmt.Printf("k=%s, i=%s\n", keyBytes, iovBytes)
+	block, err := des.NewTripleDESCipher(keyBytes)
+	if err != nil {
+		return nil, err
+	}
+	bufBase64 := base64.StdEncoding.EncodeToString(buf)
+	padBytes := noStdPadding([]byte(bufBase64), block.BlockSize())
+	mode := cipher.NewCBCEncrypter(block, iovBytes)
+	dst := make([]byte, len(padBytes))
+	mode.CryptBlocks(dst, padBytes)
+
+	return dst, nil
+}
+
+func PacketDecrypt(key string, buf []byte) ([]byte, error) {
+	keyBytes, iovBytes, err := getPacketKey(key)
+	if err != nil {
+		return nil, err
+	}
+	block, err := des.NewTripleDESCipher(keyBytes)
+	if err != nil {
+		return nil, err
+	}
+	mode := cipher.NewCBCDecrypter(block, iovBytes)
+	dst := make([]byte, len(buf))
+	mode.CryptBlocks(dst, buf)
+	unpadBytes := noStdUnpadding(dst)
+	decBytes, err := base64.StdEncoding.DecodeString(string(unpadBytes))
+	if err != nil {
+		return nil, err
+	}
+
+	return decBytes, nil
+}
+
+func PswEncrypt(pass string, signacct string) (string, error) {
 	return "buf", nil
 }
 
-func (c *Crypt) PswDecrypt(encstr string, signacct string) (string, error) {
+func PswDecrypt(encstr string, signacct string) (string, error) {
 	return "buf", nil
 }
 
-func (c *Crypt) DBDecrypt(buf string) (string, error) {
+func DBDecrypt(buf string) (string, error) {
 	orig, err := base64.StdEncoding.DecodeString(buf)
 	if err != nil {
 		return "", nil
 	}
-	key := c.getDBKey()
+	key := getDBKey()
 	block, err := des.NewCipher([]byte(key))
 	if err != nil {
 		return "", err
@@ -118,8 +187,8 @@ func (c *Crypt) DBDecrypt(buf string) (string, error) {
 	return string(dst), nil
 }
 
-func (c *Crypt) DBEncrypt(buf string) (string, error) {
-	key := c.getDBKey()
+func DBEncrypt(buf string) (string, error) {
+	key := getDBKey()
 	block, err := des.NewCipher([]byte(key))
 	if err != nil {
 		return "", err
--- util/crypt_test.go
+++ util/crypt_test.go
@@ -1,21 +1,21 @@
 package util
 
 import (
+	"encoding/base64"
 	"fmt"
 	"testing"
 )
 
-func TestCrypt(t *testing.T) {
-	c := Crypt{}
+func TestDBCrypt(t *testing.T) {
 	encStr := "cmnsf+KOk/kzY5m7nIBCzA=="
-	decStr, err := c.DBDecrypt(encStr)
+	decStr, err := DBDecrypt(encStr)
 	if err != nil {
 		fmt.Printf("ERR=%s\n", err)
 		t.FailNow()
 	}
 	fmt.Printf("decrypt:= %s\n", decStr)
 
-	newEncStr, err := c.DBEncrypt(decStr)
+	newEncStr, err := DBEncrypt(decStr)
 	if err != nil {
 		fmt.Printf("ERR=%s\n", err)
 		t.FailNow()
@@ -24,4 +24,25 @@ func TestCrypt(t *testing.T) {
 	if encStr != newEncStr {
 		t.FailNow()
 	}
+}
+
+func TestPacketCrypt(t *testing.T) {
+	key := "Q09NLkNPTkdYSU5H"
+	rawStr := "12345678---===---"
+	encStr, err := PacketEncrypt(key, []byte(rawStr))
+	if err != nil {
+		fmt.Printf("ERR=%s\n", err)
+		t.FailNow()
+	}
+	fmt.Printf("encrypt base64:= %s\n", base64.StdEncoding.EncodeToString(encStr))
+
+	decByte, err := PacketDecrypt(key, []byte(encStr))
+	if err != nil {
+		fmt.Printf("ERR=%s\n", err)
+		t.FailNow()
+	}
+
+	// X/M0ZQcfCYYLgp4wqf7xWg==
+	// X/M0ZQcfCYaXLrSRzl4Uuw==
+	fmt.Printf("decrypt:= %s\n", string(decByte))
 }
